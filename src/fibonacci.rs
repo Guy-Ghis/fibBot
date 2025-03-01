c3a16fe8586ba630cdf62a10638f4757ae6a8223
Guy-Ghis <tissongguyghislain001@gmail.com>
2025-03-01 21:45:24 +0100

    update fibonacci.rs

--- src/fibonacci.rs
+++ src/fibonacci.rs
@@ -1,24 +1,3 @@
-// pub fn fib(n: i128) -> i128 {
-
-//     if n == 0 {
-//         return 0;
-//     }
-//     if n == 1 {
-//         return 1;
-//     }
-//     if n < 0 {
-//         return fib(n + 2) - fib(n + 1);
-//     }
-//     let mut prev = 0;
-//     let mut curr = 1;
-//     for _ in 0..n {
-//         let next = prev + curr;
-//         prev = curr;
-//         curr = next;
-//     }
-//     prev
-// }
-
 use num_bigint::{BigInt, ToBigInt};
 
 pub fn fib(c: u128) -> BigInt {
