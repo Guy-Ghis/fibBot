9ff1d04f6d77cc4303fa9852b8fdc3c91fff9d99
Guy-Ghis <tissongguyghislain001@gmail.com>
2025-03-01 21:40:56 +0100

    update fibonacci.rs

--- src/fibonacci.rs
+++ src/fibonacci.rs
@@ -23,7 +23,7 @@ use num_bigint::{BigInt, ToBigInt};
 
 pub fn fib(c: u128) -> BigInt {
     match c {
-        o => 0.to_bigint().expect("Invalid input"),
+        0 => 0.to_bigint().expect("Invalid input"),
         1 => 1.to_bigint().expect("Invalid input"),
         _ => fib(c - 1) + fib(c - 2),
     }
